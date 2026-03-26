8a8ce69e23942d01cd3340ee536f6b0c8d9ff295
ayushsingh9334940651-beep <ayushsingh9334940651@gmail.com>
2026-03-27 05:07:41 +0530

    added hello.java

---
+++ hello.java
@@ -0,0 +1,22 @@
+package BasicJava;
+public class hello {
+    public static void main(String[] args) 
+    { 
+    System.out.print ("Hello World\n");
+    System.out.print ("Hello World\n");
+    System.out.print ("Hello World\n");
+    System.out.println("hello World");
+     System.out.println("hello World");
+     //next line ke lie \n  or println use krt hai,  eka code second code ki practice lka hai
+     int a=10;
+     int b=5;
+     int sum=a+b;
+     System.out.println(sum);
+     String naame = "Ayush singh";
+     System.out.println(naame);
+     System.out.println("a");
+     System.out.println(a);
+      b=55;
+     System.out.println(b); 
+ }   
+}
