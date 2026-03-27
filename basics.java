0b34a4c7f92f1d7f5c55a024fc0f4a4841e09e35
ayushsingh9334940651-beep <ayushsingh9334940651@gmail.com>
2026-03-27 23:53:19 +0530

    adding 2 file of basic java

---
+++ basics.java
@@ -0,0 +1,15 @@
+package BasicJava;
+import java.util.*;
+public class basics {
+public static void main(String[] args){
+Scanner Sec= new Scanner(System.in);
+String name=Sec.nextLine();
+System.out.println(name);
+
+int number= Sec.nextInt();
+System.out.println(number);
+
+ Sec.close();
+}     
+}
+
---
+++ input.java
@@ -0,0 +1,24 @@
+package BasicJava;
+import java.util.*;
+public class input {
+public static void main(String[] args) {
+    Scanner sec=new Scanner(System.in);
+   /*  int a=sec.nextInt();
+    float b=sec.nextFloat();
+    float sum=a+b;
+    System.out.println(sum);
+    sec.close();*/
+    int c=sec.nextInt();
+    float d=sec.nextFloat();
+    float mul=c*d;
+    System.out.println(mul);
+    float e=5.222f;
+    int f=(int)e;
+    System.out.println(f);  
+    char ch='b';
+    int number= ch;
+    System.out.println(number);
+    sec.close();
+}
+    
+}
